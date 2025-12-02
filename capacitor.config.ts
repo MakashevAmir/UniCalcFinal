74d65da295797749cc83e6644b146476f6d17ec2
Amir Makashev <a_makashev@utb.cz>
2025-12-02 16:30:42 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MenPrev',
+  webDir: 'www'
+};
+
+export default config;
